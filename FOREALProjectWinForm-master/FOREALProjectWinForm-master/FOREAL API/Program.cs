4ba96984de9774f91b0764e3a4357ec4000b184e
MuhammadFajrulfalaqIS <117189822+MuhammadFajrulfalaqIS@users.noreply.github.com>
2023-12-18 16:48:48 +0700

    Add files via upload

---
+++ FOREALProjectWinForm-master/FOREALProjectWinForm-master/FOREAL API/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Middleware untuk redirect ke URL Swagger saat mengakses URL root
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/")
+    {
+        context.Response.Redirect("/swagger");
+    }
+    else
+    {
+        await next();
+    }
+});
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
